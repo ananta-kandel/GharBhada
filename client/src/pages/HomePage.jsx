1879b3acb4e877b95db4d3894ee31e65a0a31e77
Ananta kandel <kandelananta12@gmail.com>
2024-10-01 22:59:08 +0545

    docker compose file

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../components/layout'
+import Layout from '../components/Layout';
 import { Home, Users, CreditCard } from 'lucide-react';
 const FeatureCard = ({ title, description, Icon }) => (
     <div className="bg-white rounded-lg shadow-md p-6">
